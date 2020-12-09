7aa24bb98517d5768359613d7b1a21f893c87190
Panfilenka Natallia <choco-cat@mail.ru>
2020-12-09 20:39:43 +0300

    init: setup environment

---
+++ src/api/covid.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const covid = () => {
+  //TODO Написать функцию по работе с API данных по ковиду, get метод https://api.covid19api.com/
+  //Описание работы с axios https://github.com/axios/axios#example
+};
