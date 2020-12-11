e0c336208796421ae0e9674d2bb14d68c864252a
Zedwag <zedwag@gmail.com>
2020-12-11 14:57:38 +0300

    feat: add flag service

--- src/api/flags.js
+++ src/api/flags.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 
-export const flags = () => {
-  //TODO Написать функцию по работе с API флагов, get метод https://restcountries.eu/rest/v2/all?fields=name;population;flag
-  //Описание работы с axios https://github.com/axios/axios#example
+//TODO добавить обработчик ошибок
+
+/*
+Список флагов
+Возвращает массив из объектов вида data[n], флаг - data[n].flag, название страны - data[n].name, население - data[n].population
+*/
+
+export const getFlags = async () => {
+    const url =  'https://restcountries.eu/rest/v2/all?fields=name;population;flag';
+    const { data: flags } = await axios.get(url);
+    return flags;
 };
--- src/components/root/index.js
+++ src/components/root/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { getCovidCountries, getSummaries } from '../../api/covid';
+import { getFlags } from "../../api/flags";
 import Block1 from '../block1';
 import CountryList from '../countryList';
-import World_map from '../World_map';
+import Block3 from '../block3';
 import Graph from '../graph';
 
 import './index.css';
@@ -10,14 +11,19 @@ import './index.css';
 const Root = () => {
   const [countries, setCountries] = useState( []);
   const [summaries, setSummaries] = useState( {});
+  const [flags, setFlags] = useState( []);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await getCovidCountries();
       const summariesResult = await getSummaries();
+      const flags = await getFlags();
+
+      console.log(flags)
 
       setCountries(result);
       setSummaries(summariesResult);
+      setFlags(flags)
     };
 
     fetchData();
@@ -28,14 +34,14 @@ const Root = () => {
   };
 
   return (
-    <div className="main-container">
-      <CountryList />
-      <World_map summaries={summaries.Countries} />
-      <div className="summary-container">
-        <Block1 summaries={summaries.Global}/>
-        <Graph />
+      <div className="main-container">
+        <CountryList />
+        <Block3 summaries={summaries.Countries} />
+        <div className="summary-container">
+          <Block1 summaries={summaries.Global}/>
+          <Graph />
+        </div>
       </div>
-    </div>
   );
 };
 
