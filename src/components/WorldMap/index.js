cea8907fc5946655584a74fa9f2a6a1dbe7872fb
Panfilenka Natallia <choco-cat@mail.ru>
2020-12-13 19:59:53 +0300

    feat: api cache, filters to components, refactoring

--- src/components/WorldMap/index.js
+++ src/components/WorldMap/index.js
@@ -2,12 +2,12 @@ import Map from './map';
 
 import './index.css';
 
-const WorldMap = ({ summaries }) => {
+const WorldMap = ({ summaries, handleClickOnCountry }) => {
   return (
     <div className="App">
-    <div id="tooltip" display="none"></div>
+    <div id="tooltip" display="none" />
       <h4>The World Map will be built here</h4>
-      <Map summaries={summaries} />
+      <Map summaries={summaries} handleClickOnCountry={handleClickOnCountry} />
     </div>
   );
 };
--- src/components/WorldMap/map/index.js
+++ src/components/WorldMap/map/index.js
@@ -1,58 +1,58 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import country from 'world-map-country-shapes';
 import './index.css';
 
-const Map = ({ summaries = [] }) => {
-    const [selectedCountries, setSelectedCountries] = useState({});
+const Map = ({summaries = [], handleClickOnCountry}) => {
+  const [selectedCountries, setSelectedCountries] = useState({});
+  const tooltip = document.getElementById("tooltip");
 
-    const mapCountries = country.map(country => {
-        const covidDataForCountry = summaries.find(summaryForCountry => summaryForCountry.CountryCode === country.id) || {};
-
-// выводим подсказки при наведении на страну
-    const onHoverCountry = (e, country) => {
-        let tooltip = document.getElementById("tooltip");
-        //todo Добавить вывод показателя, который используется в данный момент
-        tooltip.innerHTML = `${covidDataForCountry.Country} 'Добавить вывод показателя, который используется в данный момент '`;
-        tooltip.style.display = "block";
-        tooltip.style.left = e.pageX + 1 + 'px';
-        tooltip.style.top = e.pageY + 1 + 'px';
-        setSelectedCountries({
-            [country.id]: !selectedCountries[country.id]
-        });
-    };
-// прячим подсказку, если вышли за пределы страны
-    const onOutCountry = (country) => {
-        var tooltip = document.getElementById("tooltip");
-        tooltip.style.display = "none";
-    }
-
-
-      return (
-        <path
-            key={country.id}
-            d={country.shape}
-            style={{
-                fill: selectedCountries[country.id] ? "tomato" : "#eee",
-                cursor: "pointer",
-                stroke: "#ccc"
-            }}
-            onMouseOver= {(evt) => onHoverCountry(evt,country)}
-            onMouseOut= {() => onOutCountry(country)}
-        >
-        </path>
-      )
+  // выводим подсказки при наведении на страну
+  const onHoverCountry = (e, country, covidDataForCountry) => {
+    //todo Добавить вывод показателя, который используется в данный момент
+    tooltip.innerHTML = `${covidDataForCountry.Country} 'Добавить вывод показателя, который используется в данный момент '`;
+    tooltip.style.display = "block";
+    tooltip.style.left = e.pageX + 1 + 'px';
+    tooltip.style.top = e.pageY + 1 + 'px';
+    setSelectedCountries({
+      [country.id]: !selectedCountries[country.id]
     });
+  };
+
+  // прячем подсказку, если вышли за пределы страны
+  const onOutCountry = (country) => {
+    tooltip.style.display = "none";
+  };
+
+  const mapCountries = country.map(country => {
+    const covidDataForCountry = summaries.find(summaryForCountry => summaryForCountry.CountryCode === country.id) || {};
 
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="500"
-            width="700"
-            viewBox="0 0 2000 1001"
-        >
-            {mapCountries}
-        </svg>
+      <path
+        key={country.id}
+        d={country.shape}
+        style={{
+          fill: selectedCountries[country.id] ? "tomato" : "#eee",
+          cursor: "pointer",
+          stroke: "#ccc"
+        }}
+        onMouseOver={(evt) => onHoverCountry(evt, country, covidDataForCountry)}
+        onMouseOut={() => onOutCountry(country)}
+        onClick={() => handleClickOnCountry(covidDataForCountry.Country)}
+      >
+      </path>
     )
+  });
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="500"
+      width="700"
+      viewBox="0 0 2000 1001"
+    >
+      {mapCountries}
+    </svg>
+  )
 };
 
 
