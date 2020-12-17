b96456b1e68d39d58412e53d0acc88b84976a47d
Panfilenka Natallia <choco-cat@mail.ru>
2020-12-17 19:33:35 +0300

    feat: add legend, zoom for map

--- src/components/graph/index.js
+++ src/components/graph/index.js
@@ -3,9 +3,10 @@ import CovidChart from './chart';
 const Graph = ({ dataWorld , filters }) => {
     return (
       <div className="graph-wrapper">
-        <h3>{filters.geography ? filters.geography : 'World'}, {filters.relative}</h3>
-        <h4>{filters.status}</h4>
-        <CovidChart dataWorld={dataWorld} status={filters.status}/>
+          <h2>Chart</h2>
+          <h3>{filters.geography ? filters.geography : 'World'}, {filters.relative}</h3>
+          <h4>{filters.status}</h4>
+          <CovidChart dataWorld={dataWorld} status={filters.status}/>
       </div>
     );
 };
--- src/components/worldMap/map/tooltip.js
+++ src/components/worldMap/map/tooltip.js
@@ -1,8 +1,38 @@
-export default function Tooltip({style, dataCountry}) {
-    return(
-        <div id="tooltip" style={style}>
-            {dataCountry.data}
-        </div>
-    ) 
-}
+const Tooltip = ({ dataCountry, customStyles }) => {
+    return (
+      <div id="tooltip" className="tooltip" style={{ display: 'none', ...customStyles }}>
+          <h5>Country: {dataCountry.Country}</h5>
+          <h6>Date: {dataCountry.Date}</h6>
+          <table>
+              <tbody>
+              <tr>
+                  <td>NewConfirmed</td>
+                  <td>{dataCountry.NewConfirmed || 0}</td>
+              </tr>
+              <tr>
+                  <td>NewDeaths</td>
+                  <td>{dataCountry.NewDeaths || 0}</td>
+              </tr>
+              <tr>
+                  <td>NewRecovered</td>
+                  <td>{dataCountry.NewRecovered || 0}</td>
+              </tr>
+              <tr>
+                  <td>TotalConfirmed</td>
+                  <td>{dataCountry.TotalConfirmed || 0}</td>
+              </tr>
+              <tr>
+                  <td>TotalDeaths</td>
+                  <td>{dataCountry.TotalDeaths || 0}</td>
+              </tr>
+              <tr>
+                  <td>TotalRecovered</td>
+                  <td>{dataCountry.TotalRecovered || 0}</td>
+              </tr>
+              </tbody>
+          </table>
+      </div>
+    );
+};
 
+export default Tooltip;
